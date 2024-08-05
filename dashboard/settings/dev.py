f2b072f6c4771cb0ca79ec4361d5f8f4952771cc
elleon003 <noelle@ygitc.com>
2024-08-05 01:16:10 -0400

    Initial Build

---
+++ dashboard/settings/dev.py
@@ -0,0 +1,22 @@
+from .base import *  # noqa
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "k!o3ltes!v4o^u24yv=g(mew5o0k&qp=!#5%pc(z07$r((p#h*"
+
+ALLOWED_HOSTS = ["*"]
+
+INSTALLED_APPS += [  # noqa
+    "django_sass",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+WAGTAIL_CACHE = False
+
+try:
+    from .local import *  # noqa
+except ImportError:
+    pass
